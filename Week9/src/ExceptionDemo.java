b5c887cd1c40f865b3946de7188999710fe0fa76
Earth Pongsakon <94043213+MadaMun@users.noreply.github.com>
2023-05-05 01:15:47 +0700

    Add files via upload

---
+++ Week9/src/ExceptionDemo.java
@@ -0,0 +1,18 @@
+public class ExceptionDemo {
+    public static void main(String[] args) {
+        try {
+            double a = Double.parseDouble(args[0]);
+            double b = Double.parseDouble(args[1]);
+            double c = Double.parseDouble(args[2]);
+            double determinant = b * b - 4 * a * c;
+            double root1 = (-b + Math.sqrt(determinant)) / (2 * a);
+            double root2 = (-b - Math.sqrt(determinant)) / (2 * a);
+            System.out.println("x1 : " + root1);
+            System.out.println("x2 : " + root2);
+        } catch (NumberFormatException ex) {
+            System.out.println("Please input data in number format only.");
+        } catch (ArrayIndexOutOfBoundsException ex) {
+            System.out.println("Please enter 3 numbers as a, b, and c respectively.");
+        }
+    }
+}
